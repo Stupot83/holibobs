4849fc41bcf19baca6c753f04ff90d448f8df37e
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-12-17 11:35:19 +0000

    added a luggage list

--- client/src/components/dashboards/HolidayDashboard.js
+++ client/src/components/dashboards/HolidayDashboard.js
@@ -47,33 +47,45 @@ export default class HolidayDashBoard extends Component {
     return (
     <div>
       <div className="travel-container">
-        <div className = "travel-header"> 
-          <p className = "travel-heading"> Holibobs</p>
-          <div className = "travel-divider"></div>
+        <div className="travel-header"> 
+          <p className="travel-heading"> HOLIBOBS</p>
+          <div className="travel-divider"></div>
         </div>
   
-      <div className = "travel-location">
-        <p className = "location-name"> Location</p>
-        <p>{this.state.holiday.location}</p>
+      <div className="travel-location">
+        <p className="location-name"> Location</p>
+        <p className="location-original">{this.state.holiday.location}</p>
       </div>
       
-      <div className = "travel-dates">
-        <div className = "start-date">
-          <p className = "start-word">Start:</p>
-          <p className = "start-date-format">{this.formatDate(this.state.holiday.startDate)}</p>
+      <div className="travel-dates">
+        <div className="start-date">
+          <p className="start-word">Start:</p>
+          <p className="start-date-format">{this.formatDate(this.state.holiday.startDate)}</p>
         </div>
-        <div className = "travel-dates__image">
-          <img src="/images/plane.png"></img>
+        <div className="travel-dates__image">
+          <img className="travel-dates__image" src="/images/plane-icon.png"></img>
         </div>
-        <div className = "end-date">
-          <p className = "end-word">End:</p>
-          <p className = "end-date-format">{this.formatDate(this.state.holiday.endDate)}</p>
+        <div className="end-date">
+          <p className="end-word">End:</p>
+          <p className="end-date-format">{this.formatDate(this.state.holiday.endDate)}</p>
         </div>
       </div>
-
-        <button onClick={this.delete}>Delete</button>
+      <div className="predicted-weather">
+        <p className="predicted-weather-text">Predicted weather:</p>
       </div>
-      <a className="back-dashboard-link" href="/dashboard">Back to DashBoard</a>
+  
+      </div>
+      <div className="holiday-nav">
+        <button className="delete-button" onClick={this.delete}>Remove my Holibob</button>
+        <a className="back-dashboard-link" href="/dashboard">Back to DashBoard</a>
+        </div>
+        <div className="suitcase-container">
+          <p className="suitcase-heading">Luggage List</p>
+          <div className="suitcase-divider"></div>
+          <p className="suitcase-text">1 X T-shirt</p>
+          <p className="suitcase-text">1 X T-shirt</p>
+          <p className="suitcase-text">1 X T-shirt</p>
+        </div>
     </div>
     );
   }
