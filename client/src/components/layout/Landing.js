fac1fb4d4a117a70f10b92bacb9d41fb8bf5fb12
Lena Krasnova <olkras03@gmail.com>
2019-12-17 10:41:13 +0000

    styling landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
+import "./Landing.scss";
 
 class Landing extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="Landing">
-            <div className="row">
-              <a href="/register" className="register">
-                Register
-              </a>
-              <a href="/login" className="login">
-                Login
-              </a>
-            </div>
-          </div>
+      <div className="landing__container">
+        <h1 className="landing-heading--sub">Welcome to</h1>
+        <h1 className="landing-heading--main">HOLIBOBS</h1>
+
+        <div className="landing__button-container">
+          <a href="/register" className="landing__button">
+            Register
+          </a>
+          <a href="/login" className="landing__button">
+            Login
+          </a>
         </div>
+        <img src="/images/suitcase.png" className="landing__image"></img>
       </div>
     );
   }
